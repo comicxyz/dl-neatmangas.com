2ab9e6e3764f02d9cfe3479539259f008846d9e4
Nasyarobby <nasyarobby@gmail.com>
2023-07-01 17:09:01 +0800

    replace png with jpg

--- src/services/neatmangas.com/getInfo.ts
+++ src/services/neatmangas.com/getInfo.ts
@@ -3,7 +3,7 @@ import GetInfoFunctionType from '../../@types/GetInfoFunctionType.js';
 const getInfo: GetInfoFunctionType = () => ({
   name: 'NeatManga',
   url: 'https://neatmanga.com',
-  imageUrl: 'image.png',
+  imageUrl: 'image.jpg',
   description: '',
   disabled: false,
   domains: ['neatmangas.com'],
